6f5386b5c2c7cd1b071719daedb6bc622c4a37c9
Russell Telen <rtelen@my.bcit.ca>
2021-03-04 09:58:32 -0800

    Initial set up of TopNav story #6

---
+++ src/components/TopNav/TopNav.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+
+const TopNav = () => {
+  return (
+    <div>
+      <h1>Top Nav</h1>
+    </div>
+  );
+};
+
+export default TopNav;
---
+++ src/components/TopNav/TopNav.stories.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import TopNav from "./TopNav";
+
+export default {
+  title: "TopNav",
+  component: TopNav,
+};
+
+export const Default = () => <TopNav />;
