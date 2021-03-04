ec5341f561a1d04567aa02d387d7a15b434d74f1
Gökay Abay <abaygokay@gmail.com>
2021-03-04 11:20:00 -0800

    style card

--- src/components/ProductItem/ProductItem.js
+++ src/components/ProductItem/ProductItem.js
@@ -1,10 +1,72 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, Fragment } from "react"
 
-const ProductItem = () => {
+import { makeStyles } from "@material-ui/core/styles"
+
+import {
+  Avatar,
+  CardHeader,
+  IconButton,
+  Typography,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+} from "@material-ui/core"
+
+import FavoriteIcon from "@material-ui/icons/Favorite"
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  header: {
+    display: "flex",
+    justifyContent: "space-between",
+  },
+})
+
+const ProductItem = ({ name, price, imageUrl }) => {
+  const classes = useStyles()
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt="Contemplative Reptile"
+          height="140"
+          image="https://cdn.motor1.com/images/mgl/Yp07j/s1/tesla-pricing-lead.jpg"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <div className={classes.header}>
+            <div>
+              <Typography variant="h5" component="h1">
+                $ 200,000
+              </Typography>
+              <Typography gutterBottom>Tesla Roadster</Typography>
+            </div>
+            <div>
+              <Typography>Vancouver, BC</Typography>
+            </div>
+          </div>
+
+          <Typography variant="body2" color="textSecondary" component="p">
+            Elon Musk's new baby. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit.
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions className={classes.header}>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <Button size="small" color="primary">
+          Contact Seller
+        </Button>
+      </CardActions>
+    </Card>
   )
 }
 
