93b9ee05bc22130a05b9e8d17453f68c92342b86
Gökay Abay <abaygokay@gmail.com>
2021-03-08 16:21:29 -0800

    #24 getOne method

--- src/network.js
+++ src/network.js
@@ -1,30 +1,40 @@
-import axios from "axios";
+import axios from "axios"
 
 // api url
-const url = "https://e725t6sisd.execute-api.us-west-1.amazonaws.com/prod";
+const url = "https://e725t6sisd.execute-api.us-west-1.amazonaws.com/prod"
 
 // @todo GET the user from context
 
 // GET all postings
 export const getAll = async () => {
   try {
-    const res = await axios.get(`${url}/postings`);
-    console.log(res.data.body);
-    return res.data.body;
+    const res = await axios.get(`${url}/postings`)
+    console.log(res.data.body)
+    return res.data.body
   } catch (error) {
-    console.error(error);
+    console.error(error)
   }
-};
+}
 
 // GET all postings by category
 export const getPostingsByCategory = async (categoryId) => {
   try {
-    const res = await axios.get(`${url}/postings/category/${categoryId}`);
-    return res.data;
+    const res = await axios.get(`${url}/postings/category/${categoryId}`)
+    return res.data
   } catch (error) {
-    console.error(error);
+    console.error(error)
   }
-};
+}
+
+// GET One
+export const getOne = async (postingId) => {
+  try {
+    const res = await axios.get(`${url}/postings/category/${postingId}`)
+    console.log(res.data)
+  } catch (error) {
+    console.log(error)
+  }
+}
 
 // POST One
 export const postOne = async ({
@@ -43,7 +53,7 @@ export const postOne = async ({
     userId: "123612836876",
     username: "johndoe69",
     email: "johndoe@gmail.com",
-  };
+  }
   try {
     const res = await axios.post(`${url}/postings`, {
       user,
@@ -54,9 +64,9 @@ export const postOne = async ({
       city,
       province,
       seller_description,
-    });
-    console.log(res);
+    })
+    console.log(res)
   } catch (error) {
-    console.error(error);
+    console.error(error)
   }
-};
+}
