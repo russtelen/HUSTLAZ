b1a48e8c9df2c0cbb101a325f07c9b70ab3c7403
Gökay Abay <abaygokay@gmail.com>
2021-03-10 09:06:57 -0800

    Change urls

--- src/layouts/PageHeader/PageHeader.js
+++ src/layouts/PageHeader/PageHeader.js
@@ -1,32 +1,32 @@
-import React, { useContext } from "react"
-import TopNav from "../../components/TopNav/TopNav"
-import { useHistory } from "react-router-dom"
-import { TopNavValueContext } from "../../context/TopNavValueContext"
+import React, { useContext } from "react";
+import TopNav from "../../components/TopNav/TopNav";
+import { useHistory } from "react-router-dom";
+import { TopNavValueContext } from "../../context/TopNavValueContext";
 
 const PageHeader = () => {
-  const { topnavValue, setTopnavValue } = useContext(TopNavValueContext)
-  const history = useHistory()
+  const { topnavValue, setTopnavValue } = useContext(TopNavValueContext);
+  const history = useHistory();
 
   const homeClicked = async () => {
-    setTopnavValue("home")
-    history.push("/posts")
-  }
+    setTopnavValue("home");
+    history.push("/posts");
+  };
 
   const profileClicked = () => {
-    setTopnavValue("profile")
-    history.push("/profile")
-  }
+    setTopnavValue("profile");
+    history.push("/dashboard");
+  };
 
   const notificationClicked = () => {
-    setTopnavValue("notifications")
-  }
+    setTopnavValue("notifications");
+  };
   return (
     <TopNav
       homeClicked={() => homeClicked()}
       profileClicked={() => profileClicked()}
       notificationClicked={() => notificationClicked()}
     />
-  )
-}
+  );
+};
 
-export default PageHeader
+export default PageHeader;
