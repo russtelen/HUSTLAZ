34118319e89bce177a231220e96f595d3c2a0bb7
Gökay Abay <abaygokay@gmail.com>
2021-03-11 15:45:41 -0800

    #69 Fetch user info

--- src/layouts/UserDetailPage/UserDetailPage.js
+++ src/layouts/UserDetailPage/UserDetailPage.js
@@ -1,11 +1,29 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
+import UserDetail from "../../components/UserDetail/UserDetail";
 import { UserContext } from "../../context/UserContext";
+import { getAllUserPostings } from "../../network";
 
 const UserDetailPage = () => {
   const { user } = useContext(UserContext);
+  const [userDetail, setUserDetail] = useState({});
 
-  console.log(user);
-  return <div></div>;
+  useEffect(() => {
+    (async () => {
+      const res = await getAllUserPostings(user.username);
+      setUserDetail(res);
+    })();
+  }, []);
+
+  const editClicked = () => {
+    // PLACEHOLDER: Allow user to edit their info
+  };
+
+  return (
+    <div className="container">
+      <h1 className="text-center m-5">My Profile</h1>
+      <UserDetail user={userDetail} editClicked={() => editClicked()} />
+    </div>
+  );
 };
 
 export default UserDetailPage;
