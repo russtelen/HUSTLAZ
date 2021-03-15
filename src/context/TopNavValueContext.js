4406a25eb95d90f1ce8054d70522ca1a5c57f47f
Russell Telen <rtelen@my.bcit.ca>
2021-03-15 11:00:49 -0700

    Clean up all context files #64

--- src/context/TopNavValueContext.js
+++ src/context/TopNavValueContext.js
@@ -1,3 +1,3 @@
-import React, { createContext } from "react"
+import { createContext } from "react";
 
-export const TopNavValueContext = createContext([])
+export const TopNavValueContext = createContext([]);
