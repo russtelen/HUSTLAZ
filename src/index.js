f210638082c78aafc9f81109d3f9ac7c0e01205e
Russell Telen <rtelen@my.bcit.ca>
2021-03-15 11:01:46 -0700

    Clean up index.js #64

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import Amplify, { Auth } from "aws-amplify";
+import Amplify from "aws-amplify";
 import config from "./config.json";
 import "../node_modules/toastr/build/toastr.min.css";
 import "../node_modules/toastr/build/toastr.min";
