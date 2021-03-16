81462d821c00c6d9f9feaf13ad1f8256b54da8cb
Gökay Abay <abaygokay@gmail.com>
2021-03-16 11:31:27 -0700

    get all user favouites method

--- src/network.js
+++ src/network.js
@@ -1,80 +1,82 @@
-import axios from 'axios'
-import { userToken } from './userAuth'
+import axios from "axios";
+import { userToken } from "./userAuth";
 
-const url = 'https://e725t6sisd.execute-api.us-west-1.amazonaws.com/prod'
-
+const url = "https://e725t6sisd.execute-api.us-west-1.amazonaws.com/prod";
 
 async function tokenHeader() {
-  const token = await userToken()
+  const token = await userToken();
   if (!token) {
-    return {}
+    return {};
   }
-  return { Authorization: `${token}` }
+  return { Authorization: `${token}` };
 }
 
 async function http({ method, path, params }) {
-  const token = await tokenHeader()
+  const token = await tokenHeader();
   const headers = {
     ...token,
-    Accept: 'application/json',
-    'Content-Type': 'application/json',
-  }
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  };
 
   try {
-    let result
-    if (method == 'get' || method == 'delete') {
-      result = await axios[method](url + path, { headers })
+    let result;
+    if (method == "get" || method == "delete") {
+      result = await axios[method](url + path, { headers });
     } else {
-      result = await axios[method](url + path, params, { headers })
+      result = await axios[method](url + path, params, { headers });
     }
 
-    return result.data
+    return result.data;
   } catch (error) {
-    throw error.response.data.error ? Error(error.response.data.error) : error
+    throw error.response.data.error ? Error(error.response.data.error) : error;
   }
 }
 
 // GET all postings
 export const getAll = async () => {
   try {
-    const res = await axios.get(`${url}/postings`)
-    return res.data.body
+    const res = await axios.get(`${url}/postings`);
+    return res.data.body;
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
-}
+};
 
 // GET all postings by category
 export const getPostingsByCategory = async (categoryId) => {
   try {
-    const res = await axios.get(`${url}/postings/category/${categoryId}`)
-    return res.data
+    const res = await axios.get(`${url}/postings/category/${categoryId}`);
+    return res.data;
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
-}
+};
 
 // GET One
 export const getOne = async (postingId) => {
   try {
-    const res = await axios.get(`${url}/postings/${postingId}`)
-    return res.data
+    const res = await axios.get(`${url}/postings/${postingId}`);
+    return res.data;
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
 
 export async function postOneImageFile(
   { title, price, file, category, city, province, seller_description },
   user
 ) {
-  let signedURLResult = await http({ method: 'get', path: '/securetoken' })
-  const { uploadURL, Key } = signedURLResult
+  let signedURLResult = await http({ method: "get", path: "/securetoken" });
+  const { uploadURL, Key } = signedURLResult;
 
-  await axios.put(uploadURL, file)
-  const image_ref = uploadURL.split('?')[0]
+  await axios.put(uploadURL, file);
+  const image_ref = uploadURL.split("?")[0];
 
-  return await postOne({ title, price, image_ref, category, city, province, seller_description }, user)
+  return await postOne(
+    { title, price, image_ref, category, city, province, seller_description },
+    user
+  );
 }
 
 // POST One
@@ -84,11 +86,11 @@ export function postOne(
 ) {
   const userObj = {
     username: user.username,
-  }
+  };
 
   return http({
-    method: 'post',
-    path: '/postings',
+    method: "post",
+    path: "/postings",
     params: {
       user: userObj,
       title,
@@ -99,42 +101,48 @@ export function postOne(
       province,
       seller_description,
     },
-  })
+  });
 }
 
 //  GET all regions
 export const getAllRegions = async () => {
   try {
-    const res = await axios.get(`${url}/regions`)
-    return res.data.body
+    const res = await axios.get(`${url}/regions`);
+    return res.data.body;
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
-}
+};
 
 //  GET all cities by region
 export const getCitiesByRegion = async (region) => {
   try {
-    const res = await axios.get(`${url}/cities/${region}`)
-    return res.data
+    const res = await axios.get(`${url}/cities/${region}`);
+    return res.data;
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
-}
+};
 
 // GET all post that belong to current user
 export function getAllUserPostings(username) {
-  return http({ method: 'get', path: `/users/${username}` })
+  return http({ method: "get", path: `/users/${username}` });
 }
 
-export async function updateOneImageFile({title, price, file, category, city, province, seller_description}, postingId) {
-  let signedURLResult = await http({ method: 'get', path: '/securetoken' })
-  const { uploadURL, Key } = signedURLResult
+export async function updateOneImageFile(
+  { title, price, file, category, city, province, seller_description },
+  postingId
+) {
+  let signedURLResult = await http({ method: "get", path: "/securetoken" });
+  const { uploadURL, Key } = signedURLResult;
 
-  await axios.put(uploadURL, file)
-  const image_ref = uploadURL.split('?')[0]
+  await axios.put(uploadURL, file);
+  const image_ref = uploadURL.split("?")[0];
 
-  return await updateOne({ title, price, image_ref, category, city, province, seller_description }, postingId)
+  return await updateOne(
+    { title, price, image_ref, category, city, province, seller_description },
+    postingId
+  );
 }
 
 // UPDATE posting
@@ -143,7 +151,7 @@ export function updateOne(
   postingId
 ) {
   return http({
-    method: 'put',
+    method: "put",
     path: `/postings/${postingId}`,
     params: {
       title,
@@ -154,10 +162,18 @@ export function updateOne(
       province,
       seller_description,
     },
-  })
+  });
 }
 
 // DELETE posting
 export function deleteOne(postingId) {
-  return http({ method: 'delete', path: `/postings/${postingId}` })
+  return http({ method: "delete", path: `/postings/${postingId}` });
 }
+
+// GET FAVOURITES
+export function getAllUserFavourites(username) {
+  return http({ method: "get", path: `/users/favourites/${username}` });
+}
+// POST FAVOURITES
+
+// DELETE FAVOURITES
