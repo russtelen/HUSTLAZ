c1fa2f2854877ec83c0db41df035ace43dd16619
Gökay Abay <abaygokay@gmail.com>
2021-03-16 16:47:29 -0700

    #87 display liked, implement delete, post

--- src/layouts/ProductsPage/ProductsPage.js
+++ src/layouts/ProductsPage/ProductsPage.js
@@ -1,17 +1,23 @@
-import React, { useEffect, useState, useContext } from "react";
-import ProductItem from "../../components/ProductItem/ProductItem";
-import ProductDetail from "../../components/ProductDetail/ProductDetail";
-import { PostsContext } from "../../context/PostsContext";
-import { useParams } from "react-router-dom";
-import { getAll, getPostingsByCategory } from "../../network";
-import Modal from "@material-ui/core/Modal";
-import Backdrop from "@material-ui/core/Backdrop";
-import Fade from "@material-ui/core/Fade";
-import { makeStyles } from "@material-ui/core/styles";
-import { IconButton, TextField, FormControl, Input, InputAdornment } from "@material-ui/core";
-import { searchPostings } from '../../network'
-import SearchIcon from '@material-ui/icons/Search';
-
+import React, { useEffect, useState, useContext } from "react"
+import ProductItem from "../../components/ProductItem/ProductItem"
+import ProductDetail from "../../components/ProductDetail/ProductDetail"
+import { PostsContext } from "../../context/PostsContext"
+import { UserContext } from "../../context/UserContext"
+import { useParams } from "react-router-dom"
+import {
+  getAll,
+  getPostingsByCategory,
+  getAllUserFavourites,
+  removeUserFavourite,
+  addUserFavourite,
+} from "../../network"
+import Modal from "@material-ui/core/Modal"
+import Backdrop from "@material-ui/core/Backdrop"
+import Fade from "@material-ui/core/Fade"
+import { makeStyles } from "@material-ui/core/styles"
+import { IconButton, TextField } from "@material-ui/core"
+import { searchPostings } from "../../network"
+import SearchIcon from "@material-ui/icons/Search"
 
 const useStyles = makeStyles((theme) => ({
   modal: {
@@ -26,124 +32,145 @@ const useStyles = makeStyles((theme) => ({
     margin: theme.spacing(1),
   },
   noPostingsCont: {
-    height: '100%',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
-}));
+    height: "100%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+}))
 
 const ProductsPage = () => {
-  const classes = useStyles();
+  const classes = useStyles()
   // Context
-  const { posts, setPosts } = useContext(PostsContext);
+  const { posts, setPosts } = useContext(PostsContext)
+  const { user } = useContext(UserContext)
 
   // Local state
-  const [postDetail, setPostDetail] = useState({});
-  const [open, setOpen] = useState(false);
-  const [didChange, setDidChange] = useState(false);
-  const [category, setCategory] = useState("");
-  const [searchValue, setSearchValue] = useState('');
+  const [postDetail, setPostDetail] = useState({})
+  const [open, setOpen] = useState(false)
+  const [didChange, setDidChange] = useState(false)
+  const [category, setCategory] = useState("")
+  const [searchValue, setSearchValue] = useState("")
+  const [favouritePosts, setFavouritePosts] = useState([])
 
   // Params :category
-  const { categoryId } = useParams();
+  const { categoryId } = useParams()
 
   // ===================================================
   // On load
   // Set post === category in the params
   // if != category in params, set post === all post
   useEffect(() => {
-    (async () => {
+    ;(async () => {
       if (categoryId !== undefined) {
-        const data = await getPostingsByCategory(categoryId);
-        setDidChange(false);
-        setPosts(data);
-        setDidChange(true);
-        return;
+        const data = await getPostingsByCategory(categoryId)
+        setDidChange(false)
+        setPosts(data)
+        setDidChange(true)
+        return
       }
 
-      setDidChange(false);
-      const allPosts = await getAll();
-      setDidChange(true);
-      setPosts(allPosts);
-    })();
+      setDidChange(false)
+      const allPosts = await getAll()
+      setDidChange(true)
+      setPosts(allPosts)
 
-    const categories = ["none", "Tops", "Bottoms", "Shoes", "Items", "Misc"];
+      const res = await getAllUserFavourites(user.username)
+      setFavouritePosts(res)
+    })()
 
-    setCategory(categoryId ? categories[categoryId] : "All Posts");
-  }, [categoryId]);
+    const categories = ["none", "Tops", "Bottoms", "Shoes", "Items", "Misc"]
+    setCategory(categoryId ? categories[categoryId] : "All Posts")
+  }, [categoryId])
+
   // ===================================================
 
   // Handlers
   const cardCliked = (post) => {
-    setPostDetail(post);
-    setOpen(true);
-  };
+    setPostDetail(post)
+    setOpen(true)
+  }
 
-  const likeCliked = (data) => {
-    // user can unlike the posting
-    // const res = await addUserFavourite(data) = data is going to be the req body comes from the product item component
-    // user can like the posting
-    // const res = await removeUserFavourite(data) = data is going to be postingId
-  };
+  const likeCliked = async ({ postingId, liked }) => {
+    if (liked) {
+      await removeUserFavourite(postingId)
+    } else {
+      await addUserFavourite(user.username, postingId)
+    }
+  }
+
+  const checkFavourite = (postId) => {
+    const found = favouritePosts.some((fav) => fav.id === postId)
+    return found
+  }
 
   const contactClicked = () => {
-    console.log("contact seller");
-  };
+    console.log("contact seller")
+  }
 
   const handleSearch = async (e) => {
     e.preventDefault()
     const res = await searchPostings(searchValue)
     setPosts(res)
-    setSearchValue('')
+    setSearchValue("")
   }
 
   return (
     <div className="container">
       <div className="row d-flex justify-content-around align-items-center">
         <h1 className="text-center mt-5">{category}</h1>
-        <form className="mt-5" onSubmit={handleSearch} noValidate autoComplete="off">
+        <form
+          className="mt-5"
+          onSubmit={handleSearch}
+          noValidate
+          autoComplete="off"
+        >
           <TextField
-          InputProps={{
-            startAdornment:
-            <IconButton onClick={handleSearch}><SearchIcon/></IconButton>,
-            classes: {
-              adornedEnd: classes.adornedStart
-            }
-          }}
+            InputProps={{
+              startAdornment: (
+                <IconButton onClick={handleSearch}>
+                  <SearchIcon />
+                </IconButton>
+              ),
+              classes: {
+                adornedEnd: classes.adornedStart,
+              },
+            }}
             className={classes.searchInput}
             onChange={(e) => setSearchValue(e.target.value)}
             value={searchValue}
-            id="search-input" 
-            label="Search by user or title" 
-            variant="outlined" />
+            id="search-input"
+            label="Search by user or title"
+            variant="outlined"
+          />
         </form>
       </div>
-      {posts.length > 0 ?
-      <div className="row d-flex justify-content-center ">
-        {posts?.map((post, idx) => (
-          <div
-            key={idx}
-            className={
-              didChange
-                ? "col-sm-12 col-md-4 mt-5 animate__animated animate__fadeIn animate__faster"
-                : ""
-            }
-          >
-            <ProductItem
-              post={{ ...post }}
-              cardClicked={() => cardCliked(post)}
-              likeClicked={() => likeCliked()}
-              contactClicked={() => contactClicked()}
-            />
-          </div>
-        ))}
-      </div>
-      : 
-      <div className={classes.noPostingsCont}>
-        <h1>No postings!</h1>
-      </div>
-      }
+      {posts.length > 0 ? (
+        <div className="row d-flex justify-content-center ">
+          {posts?.map((post, idx) => (
+            <div
+              key={idx}
+              className={
+                didChange
+                  ? "col-sm-12 col-md-4 mt-5 animate__animated animate__fadeIn animate__faster"
+                  : ""
+              }
+            >
+              <ProductItem
+                post={{ ...post }}
+                cardClicked={() => cardCliked(post)}
+                likeClicked={(data) => likeCliked(data)}
+                contactClicked={() => contactClicked()}
+                favourite={checkFavourite(post.id)}
+              />
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div className={classes.noPostingsCont}>
+          <h1>No postings!</h1>
+        </div>
+      )}
       <div>
         <Modal
           aria-labelledby="transition-modal-title"
@@ -167,7 +194,7 @@ const ProductsPage = () => {
         </Modal>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default ProductsPage;
+export default ProductsPage
