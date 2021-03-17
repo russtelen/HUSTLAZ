65ab982867988cd70112a521982485d17e19c207
Russell Telen <rtelen@my.bcit.ca>
2021-03-17 16:25:23 -0700

    Fix set state bug #92

--- src/components/Developers/Developers.js
+++ src/components/Developers/Developers.js
@@ -2,8 +2,29 @@ import React from "react"
 
 const Developers = ({ developer }) => {
   return (
-    <div>
-      <h1>{developer.name}</h1>
+    <div className="container">
+      {!developer ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <h1 className="text-center mt-3">{developer.name}</h1>
+          <div className="my-5">
+            <div className="row">
+              <div className="col-6">
+                <img src={developer.img} />
+              </div>
+              <div className="col-6">
+                <div className="row">
+                  <div>
+                    <h3>About</h3>
+                    <p>{developer.about}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>{" "}
+        </>
+      )}
     </div>
   )
 }
