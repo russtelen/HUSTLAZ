e5bc11203b5c9836d3a96a156bec27fb7834ccef
Gökay Abay <abaygokay@gmail.com>
2021-03-17 17:12:27 -0700

    Update for context provider

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
-import React, { useState, useMemo } from "react"
+import React from "react"
 import { BrowserRouter as Router } from "react-router-dom"
 import PageHeader from "./layouts/PageHeader/PageHeader"
 import DashBoardPage from "./layouts/DashBoardPage/DashBoardPage"
-import { PostsContext } from "./context/PostsContext"
-import { PageCountContext } from "./context/PageCountContext"
-import { TopNavValueContext } from "./context/TopNavValueContext"
-import { UserContext } from "./context/UserContext"
-import { LoginFormContext } from "./context/LoginFormContext"
-import { currentUser } from "./userAuth"
-
 import { createMuiTheme } from "@material-ui/core/styles"
 import { ThemeProvider } from "@material-ui/styles"
-import { EditPostContext } from "./context/EditPostContext"
+import ContextProvider from "./context/ContextProvider"
 
 const THEME = createMuiTheme({
   typography: {
@@ -24,51 +17,13 @@ const THEME = createMuiTheme({
 })
 
 function App() {
-  const [posts, setPosts] = useState([])
-  const value = useMemo(() => ({ posts, setPosts }), [posts, setPosts])
-
-  const [pageCount, setPageCount] = useState([])
-  const pageCountValue = useMemo(() => ({ pageCount, setPageCount }), [
-    pageCount,
-    setPageCount,
-  ])
-
-  const [topnavValue, setTopnavValue] = useState([])
-  const topnavValueContext = useMemo(() => ({ topnavValue, setTopnavValue }), [
-    topnavValue,
-    setTopnavValue,
-  ])
-  const [tabValue, setTabValue] = useState([])
-  const tabValueContext = useMemo(() => ({ tabValue, setTabValue }), [
-    tabValue,
-    setTabValue,
-  ])
-  const [user, setUser] = useState(currentUser())
-  const userValueContext = useMemo(() => ({ user, setUser }), [user, setUser])
-
-  const [editPost, setEditPost] = useState({})
-  const editPostContext = useMemo(() => ({ editPost, setEditPost }), [
-    editPost,
-    setEditPost,
-  ])
-
   return (
     <ThemeProvider theme={THEME}>
       <Router>
-        <PageCountContext.Provider value={pageCountValue}>
-          <UserContext.Provider value={userValueContext}>
-            <LoginFormContext.Provider value={tabValueContext}>
-              <PostsContext.Provider value={value}>
-                <TopNavValueContext.Provider value={topnavValueContext}>
-                  <EditPostContext.Provider value={editPostContext}>
-                    <PageHeader />
-                    <DashBoardPage />
-                  </EditPostContext.Provider>
-                </TopNavValueContext.Provider>
-              </PostsContext.Provider>
-            </LoginFormContext.Provider>
-          </UserContext.Provider>
-        </PageCountContext.Provider>
+        <ContextProvider>
+          <PageHeader />
+          <DashBoardPage />
+        </ContextProvider>
       </Router>
     </ThemeProvider>
   )
